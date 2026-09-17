5a9c3a6401b2cd28c72def452099363efe21361b
OM TIWARI <omtiwari197707@gmail.com>
2026-09-18 01:26:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.10%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) return x;
+
+        long long low = 1;
+        long long high = x / 2;
+        long long ans = 0;
+
+        while (low <= high) {
+            long long mid = (low + high) / 2;
+
+            if (mid * mid <= x) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
